7aa9a3ee938212f93b6e09bf17032b09170faa14
Gaddour <mannai.abdlkader@gmail.com>
2024-04-20 23:05:33 +0100

    seeding tasks

--- database/seed.js
+++ database/seed.js
@@ -3,6 +3,8 @@ const prisma = require("../database/prisma.js");
 const users = require("../data/users.json");
 const tasks = require("../data/tasks.json");
 
-
 (async () =>
   await prisma.user.createMany({ data: users, skipDuplicates: true }))();
+
+(async () =>
+  await prisma.task.createMany({ data: tasks, skipDuplicates: true }))();
