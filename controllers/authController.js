9489c574e2a1c14e50ad3c6efeb97fb5b1e14791
Gaddour <mannai.abdlkader@gmail.com>
2024-04-21 13:58:01 +0100

    Bcrypt password before saving it to db

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const { user } = require("../database/prisma");
+const bcrypt = require("bcrypt");
 
 // import User prisma Model
 const User = require("../database/prisma").user;
@@ -6,7 +7,18 @@ const User = require("../database/prisma").user;
 //? Register Handler
 const register = async (req, res) => {
   try {
-    const response = await User.create({ data: req.body });
+    // deconstruct password for hashing
+    const { password } = req.body;
+    // copy req.body
+    const data = { ...req.body };
+
+    // hash password
+    data.password = bcrypt.hashSync(password, 8);
+
+    // execute query
+    const response = await User.create({ data });
+
+    // sernd query response
     res.send(response);
   } catch (error) {
     console.log(error);
