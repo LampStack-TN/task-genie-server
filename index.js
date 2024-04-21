0e49fe3bae852a0360de95425bd0ddfdcacc9aff
ahmedGDI <guedria.guedri@gmail.com>
2024-04-21 17:28:34 +0100

    add morgan & corse middlewares

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // importing dependencies
 const express = require("express");
-
+var morgan = require('morgan')
+var cors = require('cors')
 // importing local dependencies
 const authRouter = require("./routes/auth");
 const taskRouter = require("./routes/Task");
-
 // app & middlewares
 const app = express();
+app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json());
 
 // defining routes
 app.use("/api/auth", authRouter);
-app.use("/api/add", taskRouter);
+app.use("/api/task", taskRouter);
 
 // app listening/serving
 app.listen(3000, () => {
