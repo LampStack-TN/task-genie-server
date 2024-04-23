090cadc75ca8e216e0f90fc057285dc6de75b9bf
selim021 <brahamselim09@gmail.com>
2024-04-23 19:11:55 +0200

    Configured Post route /getAllApp to handle the getAllApp function from applicationController

--- routes/application.js
+++ routes/application.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-const { applyToTask} = require('../controllers/ApplicationController.js'); 
+const { applyToTask,getAllApp} = require('../controllers/ApplicationController.js'); 
 
 router.post('/apply', applyToTask);
-
+router.get("/getAllApp",getAllApp)
 module.exports = router;
