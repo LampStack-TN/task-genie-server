827984ba975ce28579d3bc58dad66c43eb973d03
selim021 <brahamselim09@gmail.com>
2024-04-23 17:08:07 +0200

    Implemented 'applyToTask' endpoint /ApplicationController

---
+++ controllers/ApplicationController.js
@@ -0,0 +1,57 @@
+const prisma = require("../database/prisma.js");
+
+const applyToTask = async (req, res) => {
+  const { userId, taskId, price } = req.body;
+  console.log(req.body);
+
+  if (!userId) {
+    return res.status(400).json({ message: "User ID is required." });
+  }
+
+  //   find the first Application hat matches the "taskId" and "userId".
+  try {
+    const existApp = await prisma.application.findFirst({
+      where: {
+        task: {
+          id: taskId,
+        },
+        applicant: {
+          id: userId,
+        },
+      },
+    });
+
+    if (existApp) {
+      return res
+        .status(409)
+        .json({ message: "You have already applied to this task." });
+    }
+
+    const application = await prisma.application.create({
+      data: {
+        task: {
+          connect: {
+            id: taskId,
+          },
+        },
+        applicant: {
+          connect: {
+            id: userId,
+          },
+        },
+        status: "Pending",
+        price: price,
+      },
+    });
+
+    return res.status(201).json({ application });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+module.exports = {
+  applyToTask
+};
