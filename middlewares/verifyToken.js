6658fb3a2e27cd94905368e8f651b05503595583
Gaddour <mannai.abdlkader@gmail.com>
2024-04-24 15:51:47 +0100

    Token verification

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = async (req, res, next) => {
+  try {
+    const authHeader = req.headers["g-token"];
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ error: "invalid Authorization header." });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+      req.userId = decoded.userId;
+    });
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ error: "failed." });
+  }
+};
+
+module.exports = verifyToken;
