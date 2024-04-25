89ae01c925cb83cc36222f962d1eb55f98f7dd09
lindamnasri <mnasrilinda20@gmail.com>
2024-04-25 11:28:17 +0100

    tweaks seed process

--- database/seed.js
+++ database/seed.js
@@ -4,25 +4,26 @@ const prisma = require("../database/prisma.js");
 // importing Mock Data
 const users = require("../data/users.json");
 const tasks = require("../data/tasks.json");
-const skills = require("../data/skills.json")
+const skills = require("../data/skills.json");
+(async () => {
+  await prisma.user
+    .createMany({ data: users, skipDuplicates: true })
+    .then((response) => {
+      console.log("Users seeded successfull ✅: ", response);
+      console.log("******************************");
+    });
 
-prisma.user
-  .createMany({ data: users, skipDuplicates: true })
-  .then((response) => {
-    console.log("Users seeded successfull ✅: ", response);
-    console.log("******************************");
-  });
+  await prisma.task
+    .createMany({ data: tasks, skipDuplicates: true })
+    .then((response) => {
+      console.log("Tasks seeded successfull ✅: ", response);
+      console.log("******************************");
+    });
 
-prisma.task
-  .createMany({ data: tasks, skipDuplicates: true })
-  .then((response) => {
-    console.log("Tasks seeded successfull ✅: ", response);
-    console.log("******************************");
-  });
-  prisma.skills
-  .createMany({ data: skills, skipDuplicates: true })
-  .then((response) => {
-    console.log("skills seeded successfull ✅: ", response);
-    console.log("******************************");
-  });
-
+  await prisma.skills
+    .createMany({ data: skills, skipDuplicates: true })
+    .then((response) => {
+      console.log("skills seeded successfull ✅: ", response);
+      console.log("******************************");
+    });
+})();
