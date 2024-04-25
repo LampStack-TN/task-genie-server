a7a4a07d38f6268a30dd412481408562a4095c39
selim021 <brahamselim09@gmail.com>
2024-04-25 15:38:59 +0200

    configured get route /myTasks to handle getMyTasks from controller

--- routes/Task.js
+++ routes/Task.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const taskController = require("../controllers/TaskContoller");
+const verifyToken = require("../middlewares/verifyToken.js");
 
 router.post("/add", taskController.CreateTask);
-router.get("/getAll",taskController.getAll)
-router.get("/getOne/:id",taskController.getOne)
-router.delete("/delete/:id",taskController.deleteTask)
-router.put("/update/:id",taskController.updateTask)
+router.get("/getAll", taskController.getAll);
+router.get("/getOne/:id", taskController.getOne);
+router.delete("/delete/:id", taskController.deleteTask);
+router.put("/update/:id", taskController.updateTask);
+router.get("/myTasks", verifyToken, taskController.getMyTasks);
 module.exports = router;
