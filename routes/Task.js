c429afd3bdc7c8524cf192d4dcc233947fbe87cc
ahmedGDI <guedria.guedri@gmail.com>
2024-04-25 17:44:26 +0100

    ad verifyToken in task route for authorization process

--- routes/Task.js
+++ routes/Task.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const taskController = require("../controllers/TaskContoller");
-
-router.post("/add", taskController.CreateTask);
-router.get("/getAll",taskController.getAll)
-router.get("/getOne/:id",taskController.getOne)
-router.delete("/delete/:id",taskController.deleteTask)
-router.put("/update/:id",taskController.updateTask)
+const verifyToken = require("../middlewares/verifyToken")
+router.post("/add",verifyToken , taskController.CreateTask);
+router.get("/getAll",verifyToken,taskController.getAll)
+router.get("/getOne/:id",verifyToken,taskController.getOne)
+router.delete("/delete/:id",verifyToken,taskController.deleteTask)
+router.put("/update/:id",verifyToken,taskController.updateTask)
 module.exports = router;
