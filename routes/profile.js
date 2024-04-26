8387c9b23eb550b5433e78d8aa423493c2a06065
lindamnasri <mnasrilinda20@gmail.com>
2024-04-26 02:10:39 +0100

    created createdPofile route

--- routes/profile.js
+++ routes/profile.js
@@ -3,5 +3,6 @@ const profileController = require("../controllers/profileController");
 const verifyToken = require("../middlewares/verifyToken");
 
 router.get("/getProfile", verifyToken, profileController.getUserProfile);
+router.post("/createProfile", verifyToken, profileController.createProfile);
 
 module.exports = router;
