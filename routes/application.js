4a68b1d0c9844d6dd01f0341b79e2b6aa9afdca5
selim021 <brahamselim09@gmail.com>
2024-04-27 17:39:47 +0200

    ading route

--- routes/application.js
+++ routes/application.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 
-const { applyToTask,getAllApp,getUserApplications} = require('../controllers/ApplicationController.js'); 
+const { applyToTask,getAllApp,getUserApplications,getTaskApplications} = require('../controllers/ApplicationController.js'); 
 const verifyToken=require('../middlewares/verifyToken.js')
 
 router.post('/apply',verifyToken, applyToTask);
 router.get("/getAllApp",getAllApp)
 router.get("/app/",verifyToken,getUserApplications)
+router.get('/:taskId/applications', verifyToken,getTaskApplications);
 module.exports = router;
