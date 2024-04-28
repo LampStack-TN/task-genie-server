f01da23f4703d3873950840a4bc5d1af0e794f41
lindamnasri <mnasrilinda20@gmail.com>
2024-04-28 19:46:22 +0100

    update user route

--- routes/auth.js
+++ routes/auth.js
@@ -14,4 +14,7 @@ router.get("/verify-token", verifyToken, authController.getAuthUser);
 //? Endpoint to set userRole
 router.put("/set-role", verifyToken, authController.setUserRole);
 
+//?Endpoint to update user Info
+router.put("/updateUser", verifyToken, authController.updateUser);
+
 module.exports = router;
