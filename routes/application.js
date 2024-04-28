b4e7a153329dd521ba7a4825d010d3be59d0d732
selim021 <brahamselim09@gmail.com>
2024-04-28 19:11:04 +0200

    configured post route /application/respond  to handle acceptOrRejectApplication from controller

--- routes/application.js
+++ routes/application.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
 
-const { applyToTask,getAllApp,getUserApplications,getTaskApplications} = require('../controllers/ApplicationController.js'); 
-const verifyToken=require('../middlewares/verifyToken.js')
+const {
+  applyToTask,
+  getAllApp,
+  getUserApplications,
+  acceptOrRejectApplication,
+  getTaskApplications,
+} = require("../controllers/ApplicationController.js");
+const verifyToken = require("../middlewares/verifyToken.js");
 
-router.post('/apply',verifyToken, applyToTask);
-router.get("/getAllApp",getAllApp)
-router.get("/app/",verifyToken,getUserApplications)
-router.get('/:taskId/applications', verifyToken,getTaskApplications);
+router.post("/apply", verifyToken, applyToTask);
+router.get("/getAllApp", getAllApp);
+router.get("/app/", verifyToken, getUserApplications);
+router.get("/:taskId/applications", verifyToken, getTaskApplications);
+router.post("/application/respond", verifyToken, acceptOrRejectApplication);
 module.exports = router;
