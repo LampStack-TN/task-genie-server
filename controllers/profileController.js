689ed4e6536cdb477ea4d507eba5434c5727e722
selim021 <brahamselim09@gmail.com>
2024-04-30 06:12:30 +1300

    handling getOneProfile in the profileController

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -15,27 +15,21 @@ const getUserProfile = async (req, res) => {
     if (user) {
       res.send(user);
     } else {
-      res
-        .status(404)
-        .send({
-          message: "User profile not found. Please check  and try again.",
-        });
+      res.status(404).send({
+        message: "User profile not found. Please check  and try again.",
+      });
     }
   } catch (error) {
-    res
-      .status(500)
-      .send({
-        message:
-          "Failed to retrieve user profile due to a server error. Please try again later.",
-      });
+    res.status(500).send({
+      message:
+        "Failed to retrieve user profile due to a server error. Please try again later.",
+    });
   }
 };
 
 const createProfile = async (req, res) => {
   try {
     var { userId } = req;
-    console.log(userId);
-    // console.log(userId);
     const userInfo = await User.findUnique({
       where: {
         id: userId,
@@ -62,13 +56,34 @@ const createProfile = async (req, res) => {
 
     res.status(201).send(profile);
   } catch (error) {
-    res
-      .status(500)
-      .send({
-        message:
-          "Failed to create profile due to a server error. Please check your data and try again.",
-      });
+    res.status(500).send({
+      message:
+        "Failed to create profile due to a server error. Please check your data and try again.",
+    });
   }
 };
 
-module.exports = { getUserProfile, createProfile };
+const getOneProfile = async (req, res) => {
+  try {
+    const { profileId } = req.params;
+    const profile = await Profile.findUnique({
+      where: {
+        id: parseInt(profileId),
+      },
+    });
+    if (profile) {
+      res.send(profile);
+    } else {
+      res
+        .status(404)
+        .send({ message: "Profile not found.Please verify and try again" });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message:
+        "An unexpected error occurred while retrieving the profile. We are working to resolve the issue promptly.",
+    });
+  }
+};
+
+module.exports = { getUserProfile, createProfile, getOneProfile };
