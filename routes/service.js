d42c71829d2da86d42e6cc411a70deb66f61f49f
lindamnasri <mnasrilinda20@gmail.com>
2024-04-30 13:01:35 +0100

    endpoint to get all services

--- routes/service.js
+++ routes/service.js
@@ -3,5 +3,6 @@ const serviceController = require("../controllers/serviceController");
 const verifyToken = require("../middlewares/verifyToken");
 
 router.post("/createService", verifyToken, serviceController.CreateService);
+router.get("/getAllServices", verifyToken, serviceController.getAll);
 
 module.exports = router;
