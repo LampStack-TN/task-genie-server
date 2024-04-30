6ce7f45bc2f50d124b75d84c184cf79394593a55
selim021 <brahamselim09@gmail.com>
2024-04-30 11:14:26 +0100

    post route /unlikeTask to handle unlikeTask

--- routes/favourite.js
+++ routes/favourite.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const {
   likeTask,
   getFavoriteTasks,
+  unlikeTask,
 } = require("../controllers/FavouriteTaskController");
 const verifyToken = require("../middlewares/verifyToken");
 
 router.post("/likeTask", verifyToken, likeTask);
 router.get("/favoriteTasks", verifyToken, getFavoriteTasks);
+router.post("/unlikeTask", verifyToken, unlikeTask);
 module.exports = router;
