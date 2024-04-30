f0abc62f24ca61f7a75bf66298970edd4e25d569
lindamnasri <mnasrilinda20@gmail.com>
2024-04-30 12:15:52 +0100

    Endpoint to create service

--- routes/service.js
+++ routes/service.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
 const serviceController = require("../controllers/serviceController");
+const verifyToken = require("../middlewares/verifyToken");
+
+router.post("/createService", verifyToken, serviceController.CreateService);
 
 module.exports = router;
