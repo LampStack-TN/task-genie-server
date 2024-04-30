8e3edd48eb6caaf3af83abebbe406d6155b88191
lindamnasri <mnasrilinda20@gmail.com>
2024-04-30 13:01:07 +0100

    get All services done

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,32 +1,54 @@
 const Service = require("../database/prisma").service;
+
 const CreateService = async (req, res) => {
   try {
     const { userId } = req;
-    const { title, description, price, avaiblity, location, skills } = req.body;
-    const resposne = await Service.create({
+    const { title, description, price, availability, location } = req.body;
+    console.log(userId);
+    const response = await Service.create({
       data: {
         professionalId: userId,
         title,
         description,
         price,
-        avaiblity,
+        availability,
         location,
-
-        skills: {
-          connect: skills.map((id) => ({ id })),
-        },
       },
-      include:{
-        skills:true
-      }
+      // skills: {
+      //   connect: skills.map((id) => ({ id })),
+      // },
+
+      include: {
+        skills: true,
+      },
     });
     res.status(201).send(response);
   } catch (error) {
     console.log(error);
-    res.status(400).send(error)
+    res.status(400).send(error);
   }
 };
 
-module.exports ={
-    CreateService
-}
+const getAll = async (req, res) => {
+  try {
+    const services = await Service.findMany({
+      include: {
+        skills: true,
+        professional: true,
+      },
+    });
+    res.json(services);
+  } catch (err) {
+    res.status(500).send({
+      message:
+        "Unable to retrieve services at this time. Please try again later.",
+    });
+  }
+};
+
+
+
+module.exports = {
+  CreateService,
+  getAll,
+};
