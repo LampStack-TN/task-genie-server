fddf8604955edcd5dd477e4f98df3f5b93391726
ahmedGDI <guedria.guedri@gmail.com>
2024-04-30 18:03:26 +0100

    invoke multer and use it

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // importing dependencies
 const express = require("express");
 var morgan = require("morgan");
+const multer = require("multer");
 var cors = require("cors");
 // importing local dependencies
 const authRouter = require("./routes/auth");
@@ -10,12 +11,13 @@ const profileRouter=require("./routes/profile")
 const search = require("./routes/search")
 const favouriteTasks=require("./routes/favourite")
 const serviceRouter=require('./routes/service')
+const upload = multer();
 // app & middlewares
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-
+app.use(upload.any());
 // defining routes
 app.use("/api/auth", authRouter);
 app.use("/api/task", taskRouter);
@@ -24,6 +26,7 @@ app.use("/api/task",search)
 app.use("/api/profile",profileRouter)
 app.use("/api/task",favouriteTasks)
 app.use("/api/service",serviceRouter)
+
 // app listening/serving
 app.listen(3000, () => {
   console.log("Express is Serving ✅ on port 3000");
