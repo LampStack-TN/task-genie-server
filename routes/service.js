6bd1a8e319cbf846eb5138d4eaf264ddca437885
lindamnasri <mnasrilinda20@gmail.com>
2024-04-30 13:09:33 +0100

    end point to get one service

--- routes/service.js
+++ routes/service.js
@@ -4,5 +4,6 @@ const verifyToken = require("../middlewares/verifyToken");
 
 router.post("/createService", verifyToken, serviceController.CreateService);
 router.get("/getAllServices", verifyToken, serviceController.getAll);
+router.get("/getOneService/:id", verifyToken, serviceController.getOneService);
 
 module.exports = router;
