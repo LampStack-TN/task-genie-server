ac8edd323e57ced9999bef3957cd7d02a7c5514a
ahmedGDI <guedria.guedri@gmail.com>
2024-05-02 10:15:37 +0100

    create rating controller

---
+++ controllers/RatingController.js
@@ -0,0 +1,12 @@
+const User = require("../database/prisma").user;
+
+const createRating = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.error(error);
+    
+  }
+};
+
+module.exports = { createRating };
