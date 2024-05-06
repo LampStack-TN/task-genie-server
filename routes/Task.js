dcf6ac15ccfbffd1e28c8a6d875689bbd4e59069
Gaddour <mannai.abdlkader@gmail.com>
2024-05-06 04:46:32 +0100

    rename some task routes

--- routes/Task.js
+++ routes/Task.js
@@ -3,11 +3,12 @@ const taskController = require("../controllers/TaskContoller");
 
 const verifyToken = require("../middlewares/verifyToken");
 
+router.get("/my-tasks", verifyToken, taskController.getMyTasks);
 router.post("/add", verifyToken, taskController.CreateTask);
 router.get("/getAll", verifyToken, taskController.getAll);
-router.get("/getOne/:id", verifyToken, taskController.getOne);
+router.get("/:id", verifyToken, taskController.getOne);
+router.get("/client/:id", verifyToken, taskController.getOneClient);
 router.delete("/delete/:id", verifyToken, taskController.deleteTask);
 router.put("/update/:id", verifyToken, taskController.updateTask);
-router.get("/myTasks", verifyToken, taskController.getMyTasks);
 
 module.exports = router;
