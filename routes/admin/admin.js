a116b05723a6472a8bf8a47c8f763f11e8bff636
selim021 <brahamselim09@gmail.com>
2024-05-07 14:43:10 +0100

    get router for handle getAllClients from controller

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-const {signin}=require('../../controllers/AdminController')
+const {signin,getAllClients}=require('../../controllers/AdminController')
 
 router.post('/signin',signin)
-
+router.get("/clients",getAllClients)
 
 
 module.exports = router;
