e4f87f513142b438d9872da94a57dc2844ffdf2c
selim021 <brahamselim09@gmail.com>
2024-05-07 14:26:51 +0100

    implement sigin process in the AdminController

---
+++ controllers/AdminController.js
@@ -0,0 +1,37 @@
+const prisma = require("../database/prisma.js");
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const admin = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (!admin) return res.status(410).json({ error: "Email doesn't exist" });
+    const likePassword = await bcrypt.compare(password, admin.password);
+    if (!likePassword)
+      return res.status(411).json({ error: "unvalid password" });
+
+    if (admin.role !== "admin") {
+      res.status(403).json({ message: "Invalid user role" });
+    } else {
+      const token = jwt.sign(
+        { id: admin.id, role: admin.role },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+      );
+
+      return res
+        .status(201)
+        .json({ message: "logged in", token: token });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+    console.log(error);
+  }
+}
+module.exports = { signin };
