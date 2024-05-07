7eac945d75bd49b3318563d30ef54ddbe4106bd4
selim021 <brahamselim09@gmail.com>
2024-05-07 15:01:42 +0100

    get route for handling getAllProfessionals

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 
-const {signin,getAllClients}=require('../../controllers/AdminController')
+const {signin,getAllClients,getAllProfessionals}=require('../../controllers/AdminController')
 
 router.post('/signin',signin)
 router.get("/clients",getAllClients)
+router.get('/professionals',getAllProfessionals)
 
 
 module.exports = router;
