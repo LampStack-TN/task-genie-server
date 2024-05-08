6befa63ed57e739f6604fe92d076c6e04a6ec4f0
selim021 <brahamselim09@gmail.com>
2024-05-08 15:20:21 +0100

    adding route get for getAllTasks

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 
-const {signin,getAllClients,getAllProfessionals,countProfessionals,countClients}=require('../../controllers/AdminController')
+const {signin,getAllClients,getAllProfessionals,countProfessionals,countClients,getAllTasks}=require('../../controllers/AdminController')
 const verifyToken = require("../../middlewares/verifyToken.js")
 router.post('/signin'/* ,verifyToken */,signin)
 router.get("/clients"/* ,verifyToken */,getAllClients)
 router.get('/professionals'/* ,verifyToken */,getAllProfessionals)
 router.get("/countProfessionals"/* ,verifyToken */,countProfessionals)
 router.get("/countClients"/* ,verifyToken */,countClients)
-
+router.get("/AllTasks",getAllTasks)
 
 module.exports = router;
