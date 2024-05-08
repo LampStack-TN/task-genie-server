aa049fc7809763ea72ee83dc724b4cea59597fbe
selim021 <brahamselim09@gmail.com>
2024-05-08 23:52:47 +0100

    security for admin routes by adding token verification middleware

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 
 const {signin,getAllClients,getAllProfessionals,countProfessionals,countClients,getAllTasks}=require('../../controllers/AdminController')
 const verifyToken = require("../../middlewares/verifyToken.js")
-router.post('/signin'/* ,verifyToken */,signin)
-router.get("/clients"/* ,verifyToken */,getAllClients)
-router.get('/professionals'/* ,verifyToken */,getAllProfessionals)
-router.get("/countProfessionals"/* ,verifyToken */,countProfessionals)
-router.get("/countClients"/* ,verifyToken */,countClients)
+router.post('/signin' ,signin)
+router.get("/clients" ,verifyToken,getAllClients)
+router.get('/professionals',verifyToken ,getAllProfessionals)
+router.get("/countProfessionals" ,verifyToken ,countProfessionals)
+router.get("/countClients" ,verifyToken ,countClients)
 router.get("/AllTasks",getAllTasks)
 
 module.exports = router;
