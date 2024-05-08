b3280c547bb3248dd87a4c1b108eee28d087fa4a
selim021 <brahamselim09@gmail.com>
2024-05-08 11:11:24 +0100

    adding get route to handle getAllTasks from AdminController

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 
-const {signin,getAllClients,getAllProfessionals,countProfessionals,countClients}=require('../../controllers/AdminController')
+const {signin,getAllClients,getAllProfessionals,countProfessionals,countClients,getAllTasks}=require('../../controllers/AdminController')
 
 router.post('/signin',signin)
+
+
 router.get("/clients",getAllClients)
 router.get('/professionals',getAllProfessionals)
 router.get("/countProfessionals",countProfessionals)
 router.get("/countClients",countClients)
-
+router.get('/AllTasks',getAllTasks)
 
 module.exports = router;
