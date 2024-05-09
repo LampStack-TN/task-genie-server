9199c173847342cefd12e5beddb50bb7e590b4b3
Gaddour <mannai.abdlkader@gmail.com>
2024-05-09 11:36:28 +0100

    rename favourite task route

--- index.js
+++ index.js
@@ -36,12 +36,12 @@ const io = new Server(server, {
 });
 
 // defining routes
+app.use("/api/favrourite-task",favouriteTasks)
 app.use("/api/auth", authRouter);
 app.use("/api/task", taskRouter);
 app.use("/api/task",applicationRouter)
 app.use("/api/task",search)
 app.use("/api/profile",profileRouter)
-app.use("/api/task",favouriteTasks)
 app.use("/api/service",serviceRouter)
 app.use("/api/hiring",hiringRouter)
 app.use("/api/rating",ratingRouter)
