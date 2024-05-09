ee7837a2a68225f34c4257b71623a74fea11274c
ahmedGDI <guedria.guedri@gmail.com>
2024-05-09 09:14:28 +0100

    Just commenting to verifyToken

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -3,10 +3,10 @@ const router = require("express").Router();
 const {signin,getAllClients,getAllProfessionals,countProfessionals,countClients,getAllTasks}=require('../../controllers/AdminController')
 const verifyToken = require("../../middlewares/verifyToken.js")
 router.post('/signin' ,signin)
-router.get("/clients" ,verifyToken,getAllClients)
-router.get('/professionals',verifyToken ,getAllProfessionals)
-router.get("/countProfessionals" ,verifyToken ,countProfessionals)
-router.get("/countClients" ,verifyToken ,countClients)
-router.get("/AllTasks",getAllTasks)
+router.get("/clients" /*,verifyToken*/,getAllClients)
+router.get('/professionals'/*,verifyToken*/ ,getAllProfessionals)
+router.get("/countProfessionals"/* ,verifyToken*/ ,countProfessionals)
+router.get("/countClients" /*,verifyToken */,countClients)
+router.get("/AllTasks"/*,verifyToken */,getAllTasks)
 
 module.exports = router;
