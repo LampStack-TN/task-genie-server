070f3542d5432182c9be8fd772735c77541e0670
Gaddour <mannai.abdlkader@gmail.com>
2024-05-10 12:55:11 +0100

    tweak_user_schema

---
+++ prisma/migrations/20240510115502_tweak_user_schema/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "zipcode" DROP NOT NULL,
+ALTER COLUMN "avatar" SET DEFAULT 'https://st3.depositphotos.com/9998432/13335/v/450/depositphotos_133351928-stock-illustration-default-placeholder-man-and-woman.jpg';
