5931ada604753a4496bcb9f7a555d2ebdf937577
Gaddour <mannai.abdlkader@gmail.com>
2024-05-11 17:23:10 +0100

    reduce upload function

--- helper/helperFunction.js
+++ helper/helperFunction.js
@@ -1,21 +1,10 @@
-const { Readable } = require("stream");
 const cloudinary = require("../utils/cloudinary");
 
-const upload = async (buffer) => {
-    const imageStream = Readable.from(buffer);
-    const cloudinaryResult = await new Promise((resolve, reject) => {
-        const upload_stream = cloudinary.uploader.upload_stream(
-            { resource_type: "image" },
-            (error, result) => {
-                if (error) reject(error);
-                else resolve(result);
-            }
-        );
-
-        imageStream.pipe(upload_stream);
-    });
-    return cloudinaryResult.secure_url;
+const upload = async (file) => {
+  const res = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+  return res.secure_url;
 };
-
 
 module.exports = { upload };
