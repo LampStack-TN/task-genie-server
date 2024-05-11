6de7283c4de64cb5cb7a8545063df8099ac84bf8
Gaddour <mannai.abdlkader@gmail.com>
2024-05-11 16:44:37 +0100

    fix json size limit

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ const { Server } = require("socket.io");
 // importing local dependencies
 const authRouter = require("./routes/auth");
 const taskRouter = require("./routes/Task");
-const applicationRouter=require("./routes/application")
-const profileRouter=require("./routes/profile")
-const search = require("./routes/search")
-const favouriteTasks=require("./routes/favourite")
-const serviceRouter=require('./routes/service')
-const hiringRouter=require('./routes/hiring')
+const applicationRouter = require("./routes/application");
+const profileRouter = require("./routes/profile");
+const search = require("./routes/search");
+const favouriteTasks = require("./routes/favourite");
+const serviceRouter = require("./routes/service");
+const hiringRouter = require("./routes/hiring");
 
-const ratingRouter=require('./routes/rating')
+const ratingRouter = require("./routes/rating");
 
 const chatRouter = require("./routes/chat");
-const adminRouter=require('./routes/admin/admin')
+const adminRouter = require("./routes/admin/admin");
 // app & middlewares
 const upload = multer();
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(upload.any());
 const server = createServer(app);
 
@@ -36,17 +36,17 @@ const io = new Server(server, {
 });
 
 // defining routes
-app.use("/api/favrourite-task",favouriteTasks)
-app.use("/api/task-application",applicationRouter)
+app.use("/api/favrourite-task", favouriteTasks);
+app.use("/api/task-application", applicationRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/task", taskRouter);
-app.use("/api/task",search)
-app.use("/api/profile",profileRouter)
-app.use("/api/service",serviceRouter)
-app.use("/api/hiring",hiringRouter)
-app.use("/api/rating",ratingRouter)
+app.use("/api/task", search);
+app.use("/api/profile", profileRouter);
+app.use("/api/service", serviceRouter);
+app.use("/api/hiring", hiringRouter);
+app.use("/api/rating", ratingRouter);
 app.use("/api/chat", chatRouter);
-app.use('/api/admin',adminRouter)
+app.use("/api/admin", adminRouter);
 // app listening/serving
 io.on("connection", (socket) => {
   console.log("A user connected");
